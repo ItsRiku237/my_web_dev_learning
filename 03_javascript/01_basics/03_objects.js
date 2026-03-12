58cb6657dad4df364f8dbe44b045cf2878ddb265
Riku Sahu <riku.sahu237@gmail.com>
2026-03-12 15:45:06 +0530

    Add files via upload

---
+++ 03_javascript/01_basics/03_objects.js
@@ -0,0 +1,21 @@
+// An object is a collection of key-value pairs.
+// key : value
+// let objectName = {
+    // key1 : value1,
+    // key2 : value2
+// }
+let o ={
+    Name : "Riku",
+    "Job code" : "5467",
+    "is_true" : "true"
+}
+
+console.log(o);
+
+// Adding Properties to Object
+o.salary = "2300crores";
+console.log(o);
+
+// Updating Object Properties
+o.salary = "2700crores";
+console.log(o);
