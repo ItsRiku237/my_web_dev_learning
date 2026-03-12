a2e2bf3623aa03018d258f9fa249f12949c48909
Riku Sahu <riku.sahu237@gmail.com>
2026-03-12 15:44:35 +0530

    Add files via upload

---
+++ 03_javascript/01_basics/02_data_types.js
@@ -0,0 +1,10 @@
+let  x = "Riku Bhai";
+let y = 23;
+let z = 2.37;
+const a = true; //true, false
+let  b = undefined; //variable declared but no value
+let c = null; //empty value
+
+console.log( x ,y ,z ,a,b ,c) //Riku Bhai 23 2.37 true undefined null
+console.log(typeof x ,typeof y,typeof z,typeof a,typeof b,typeof c) //Used to check data type of a variable
+//output : string number number boolean undefined object
