8105515b42643d9229368db06cd6d4028a205749
Riku Sahu <riku.sahu237@gmail.com>
2026-03-13 21:07:57 +0530

    Add files via upload

---
+++ 03_javascript/01_basics/04_conditiional_statement.js
@@ -0,0 +1,41 @@
+console.log("CONDITIONAL STATEMENT")
+
+let age = 20;
+let grace = 3;
+
+console.log("Arithmetc Operators")
+
+console.log(age)
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age ** grace)
+console.log(age % grace)
+console.log(age++)
+console.log(age--)
+
+
+let a = 3;
+let b = "3";
+let c = "3";
+
+console.log("Assigrment Operators")
+console.log(age += grace)
+console.log(age -= grace)
+console.log(age *= grace)
+console.log(age /= grace)
+console.log(age %= grace)
+console.log(age **= grace)
+
+console.log("Comparison Operators")
+console.log(a == b) //equal to 
+console.log(a != b) //not equal 
+console.log(a === b)
+console.log(b === c) //equal value and type
+console.log(b !== c) //not equal value and not equal type
+
+console.log("Logical Operator")
+console.log(a != b && b != c)
+console.log(a != b || b == c)
+// console.log( a!=b ! b!=c)
