ba7944e80e14db5d28932a54bcda3e83c3a18ab3
Riku Sahu <riku.sahu237@gmail.com>
2026-03-13 21:09:57 +0530

    Create 01_if_else_statement.js

---
+++ 03_javascript/02_conditionals/01_if_else_statement.js
@@ -0,0 +1,30 @@
+console.log("CONDITIONAL STATEMENT")
+
+let age = 20;
+let grace = 3;
+
+
+if (age > 18) {
+    console.log("You can drive");
+}
+else if (age == 0) {
+    console.log("Are u lidding")
+}
+else {
+    console.log("You can not drive")
+}
+
+a = 5;
+b = 6;
+let d = a > b ? (a - b) : (b - a)
+console.log(d)
+
+/*
+translate to :
+if(a>b){
+    let d = a-b
+    }
+else{
+    let d = b-a
+    }
+*/
