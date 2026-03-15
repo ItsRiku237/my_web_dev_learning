e02df49806c92eda190dee5619b1a429a059ef72
Riku Sahu <riku.sahu237@gmail.com>
2026-03-15 21:11:39 +0530

    Implement basic array functionality in 01_array_basic.js
    Add basic array operations and logging in JavaScript

---
+++ 03_javascript/05_array/01_array_basic.js
@@ -0,0 +1,13 @@
+// Arrays are Variabls which can holt more than one value
+let arr = [2, 3, 7, 9]
+
+console.log(arr)
+
+arr[0] = 23; // array are muteable
+console.log(arr.length)//length of the array
+
+console.log(typeof(arr))//type of array
+
+console.log(arr[0])
+console.log(arr[4])
+
